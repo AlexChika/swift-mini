97771bbefefaf1b0367e7cd80843a58cc94a1aeb
Alex Chika <mcluckey1@gmail.com>
2023-11-15 03:51:23 +0100

    added swift logo  and favicon

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -8,7 +9,8 @@ export default function Home() {
   console.log({ session });
 
   return (
-    <main className="red">
+    <main className="red p-5">
+      <Image src={"/icon.png"} alt="swift logo" width={70} height={60} />
       {session?.user ? (
         <button onClick={() => signOut()}> Sign Out</button>
       ) : (
