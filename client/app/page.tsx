83bc522c5937a05331498cbfd20bc30ffe3b5fbf
Alex Chika <mcluckey1@gmail.com>
2023-11-16 01:30:55 +0100

    modiified nextauth config

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -10,11 +10,12 @@ export default function Home() {
 
   return (
     <main className="red p-5">
-      <Image src={"/icon.png"} alt="swift logo" width={70} height={60} />
       {session?.user ? (
         <button onClick={() => signOut()}> Sign Out</button>
       ) : (
-        <button onClick={() => signIn("google")}> Sign In</button>
+        <button onClick={() => signIn("google")}>
+          <Image src={"/icon.png"} alt="swift logo" width={70} height={60} />
+        </button>
       )}
     </main>
   );
