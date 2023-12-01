30a4291901202de90b0ed25237c8631f13944385
Alex Chika <mcluckey1@gmail.com>
2023-12-01 08:19:32 +0100

    test5

--- server/api/index.ts
+++ server/api/index.ts
@@ -70,8 +70,4 @@ app.use(
   })
 );
 
-const PORT = 4000;
-// Now that our HTTP server is fully set up, we can listen to it
-httpServer.listen(PORT, () => {
-  console.log(`Server is now running on http://localhost:${PORT}/graphql`);
-});
+export default httpServer;
