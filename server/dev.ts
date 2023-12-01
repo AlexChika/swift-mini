fa91fc6d9d46c1c6da75d3f630a6ac84b49a0c15
Alex Chika <mcluckey1@gmail.com>
2023-12-01 08:53:28 +0100

    all good

--- server/dev.ts
+++
@@ -1,8 +0,0 @@
-import httpServer from "./api";
-
-const PORT = 4000;
-
-// Now that our HTTP server is fully set up, we can listen to it
-httpServer.listen(PORT, () => {
-  console.log(`Server is now running on http://localhost:${PORT}/graphql`);
-});
