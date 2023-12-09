2339dbc99115a5e43ee6685bf025649db66ce498
Alex Chika <mcluckey1@gmail.com>
2023-12-09 14:55:00 +0100

    creatd conversation mutation

--- server/src/graphql/resolvers/conversations.ts
+++ server/src/graphql/resolvers/conversations.ts
@@ -1,5 +1,6 @@
-// import { GraphQLError } from "graphql";
+import { GraphQLError } from "graphql";
 import { GraphqlContext } from "../../../lib/swift-mini";
+import { Prisma } from "@prisma/client";
 
 const conversationResolver = {
   Query: {},
@@ -10,12 +11,58 @@ const conversationResolver = {
       ctx: GraphqlContext
     ) {
       const { prisma, session } = ctx;
-      const participantIds = args.participantIds;
+      const { participantIds } = args;
+      const { id: userId } = session.user;
 
-      console.log({ participantIds, session });
+      if (!session?.user) throw new GraphQLError("User is not authenticated");
+
+      try {
+        const conversation = prisma.conversation.create({
+          data: {
+            participants: {
+              createMany: {
+                data: participantIds.map((id) => {
+                  return {
+                    userId: id,
+                    hasSeenLatestMessage: id === userId,
+                  };
+                }),
+              },
+            },
+          },
+
+          include: {
+            participants: {
+              include: {
+                user: {
+                  select: {
+                    id: true,
+                    username: true,
+                  },
+                },
+              },
+            },
+            latestMessage: {
+              include: {
+                sender: {
+                  select: {
+                    id: true,
+                    username: true,
+                  },
+                },
+              },
+            },
+          },
+        });
+      } catch (error) {
+        console.log("createConversation error", error);
+        throw new GraphQLError(error?.message);
+      }
     },
   },
   // Subscription: {},
 };
 
+export const conversationsPopulated =
+  Prisma.validator<Prisma.ConversationInclude>()({});
 export default conversationResolver;
