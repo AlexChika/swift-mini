4266823f680dd567e67291afe56e4c51c9668f48
Alex Chika <mcluckey1@gmail.com>
2023-12-09 16:45:22 +0100

    ...

--- client/components/chat/feeds/index.tsx
+++ client/components/chat/feeds/index.tsx
@@ -1,11 +1,28 @@
+import { Center, Flex, Text } from "@chakra-ui/react";
 import { Session } from "next-auth";
+import { useSearchParams } from "next/navigation";
 
 type Props = {
   session: Session;
 };
 
 function Feeds({ session }: Props) {
-  return <div>Feeds</div>;
+  const search = useSearchParams();
+  const id = search.get("conversationId");
+  console.log({ id });
+
+  if (!id)
+    return (
+      <Center w="100%">
+        <Text>No conversation selected</Text>
+      </Center>
+    );
+
+  return (
+    <Flex w="100%" border="1px solid red">
+      <Text>{id}</Text>
+    </Flex>
+  );
 }
 
 export default Feeds;
