5460a2b7c3209d7699913340aa238cd65f875805
Alex Chika <mcluckey1@gmail.com>
2025-08-11 14:29:01 +0100

    Upgrade: upgraded to next-auth v5 beta

--- client/lib/api/nextAuth.ts
+++ client/lib/api/nextAuth.ts
@@ -1,16 +1,14 @@
-import { NextAuthOptions } from "next-auth";
+import { PrismaClient } from "@prisma/client";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth, { type NextAuthConfig } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 // add the below to generator fun in prisma schema
 //   output = "app/generated/prisma/client";
 // import { PrismaClient } from "@/prisma/app/generated/prisma/client"; for v7 (not supported by nextAuth yet)
 
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaClient } from "@prisma/client";
-
 const prisma = new PrismaClient();
-
-export const getAuthOptions = (): NextAuthOptions => {
+export const getAuthConfig = (): NextAuthConfig => {
   const dev = process.env.NODE_ENV === "development";
   const domain = "globalstack.dev";
   const cookiePrefix = "__Secure-";
@@ -70,3 +68,78 @@ export const getAuthOptions = (): NextAuthOptions => {
         }
   };
 };
+
+export const { auth, handlers, signIn, signOut } = NextAuth(getAuthConfig());
+
+// import { NextAuthOptions } from "next-auth";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+
+// // add the below to generator fun in prisma schema
+// //   output = "app/generated/prisma/client";
+// // import { PrismaClient } from "@/prisma/app/generated/prisma/client"; for v7 (not supported by nextAuth yet)
+
+// import GoogleProvider from "next-auth/providers/google";
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// export const getAuthOptions = (): NextAuthOptions => {
+//   const dev = process.env.NODE_ENV === "development";
+//   const domain = "globalstack.dev";
+//   const cookiePrefix = "__Secure-";
+
+//   return {
+//     adapter: PrismaAdapter(prisma),
+//     providers: [
+//       GoogleProvider({
+//         clientId: process.env.GOOGLE_CLIENT_ID!,
+//         clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+//       })
+//     ],
+//     secret: process.env.NEXTAUTH_SECRET,
+//     callbacks: {
+//       async session({ session, user, token }) {
+//         return {
+//           ...session,
+//           user: {
+//             ...session.user,
+//             ...user
+//           }
+//         };
+//       }
+//     },
+//     cookies: dev
+//       ? {}
+//       : {
+//           sessionToken: {
+//             name: `${cookiePrefix}next-auth.session-token`,
+//             options: {
+//               httpOnly: true,
+//               sameSite: "lax",
+//               path: "/",
+//               secure: true,
+//               domain
+//             }
+//           },
+//           callbackUrl: {
+//             name: `${cookiePrefix}next-auth.callback-url`,
+//             options: {
+//               sameSite: "lax",
+//               path: "/",
+//               secure: true,
+//               domain
+//             }
+//           },
+//           csrfToken: {
+//             name: `${cookiePrefix}next-auth.csrf-token`,
+//             options: {
+//               httpOnly: true,
+//               sameSite: "lax",
+//               path: "/",
+//               secure: true,
+//               domain
+//             }
+//           }
+//         }
+//   };
+// };
