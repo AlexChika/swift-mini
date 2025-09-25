f34fb4a43dabd761f9dc726df0c5c879ff34ec80
Alex Chika <mcluckey1@gmail.com>
2025-09-25 19:10:33 +0100

    DEV(fix): configuring prisma client for vercel deployment
    approach: generated prisma is now part of code

--- client/next.config.js
+++ client/next.config.js
@@ -3,21 +3,21 @@
 const path = require("path");
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-  experimental: {
-    optimizePackageImports: ["@chakra-ui/react", "@emotion/react", "@zag-js"]
-  }
+  enabled: process.env.ANALYZE === "true"
+  // experimental: {
+  //   optimizePackageImports: ["@chakra-ui/react", "@emotion/react", "@zag-js"]
+  // }
 });
 
-const nextConfig = {
+const nextConfig = withBundleAnalyzer({
   typedRoutes: true,
   experimental: {
     globalNotFound: true
+  },
+  turbopack: {
+    root: path.join(__dirname),
+    outputFileTracingRoot: path.join(__dirname)
   }
-  // turbopack: {
-  //   root: path.join(__dirname),
-  //   outputFileTracingRoot: path.join(__dirname)
-  // }
-};
+});
 
 module.exports = nextConfig;
