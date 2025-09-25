5137667e30427c19d7d0d75d331876c4fd5b78b9
Alex Chika <mcluckey1@gmail.com>
2025-09-25 12:29:58 +0100

    DEV(fix): configuring prisma for vercel deployments
    added a copyscript to package.json to copy the prisma binary to the .next/server directory after build

--- client/lib/api/nextAuth.ts
+++ client/lib/api/nextAuth.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/prisma/swift/client";
+import { PrismaClient } from "@/prisma/swift";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth, { type NextAuthConfig } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 export const getAuthConfig = (): NextAuthConfig => {
   const dev = process.env.NODE_ENV === "development";
   const domain = "globalstack.dev";
-  const cookiePrefix = "__Secure-swift-next-auth";
+  const cookiePrefix = "__Secure-swift";
 
   return {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
