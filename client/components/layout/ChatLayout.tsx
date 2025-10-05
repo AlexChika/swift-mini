3f315d0c763cfb4b29e00b5f559b13f46e3e195d
Alex Chika <mcluckey1@gmail.com>
2025-10-05 08:47:34 +0100

    fix(sprint-4): testing hack 2, ficing the whole UI on mobile

--- client/components/layout/ChatLayout.tsx
+++ client/components/layout/ChatLayout.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useRef } from "react";
+// import { useRef } from "react";
 import { Session } from "next-auth";
 import { Flex } from "@chakra-ui/react";
 import Home from "@/components/home/Home";
 import InProgressModal from "../general/InProgressModal";
-import useDynamicHeight from "@/lib/hooks/useDynamicHeight";
+// import useDynamicHeight from "@/lib/hooks/useDynamicHeight";
 
 type ChatProps = {
   session: Session;
@@ -13,17 +13,23 @@ type ChatProps = {
 };
 
 function ChatLayout({ session, children }: ChatProps) {
-  const containerRef = useRef<HTMLDivElement>(null);
-  useDynamicHeight({
-    ref: containerRef
-  });
+  // const containerRef = useRef<HTMLDivElement>(null);
+  // useDynamicHeight({
+  //   ref: containerRef
+  // });
 
   return (
     <>
       {/* temp addition */}
       <InProgressModal></InProgressModal>
 
-      <Flex gap={0} w={"100%"} margin={0} ref={containerRef}>
+      <Flex
+        gap={0}
+        w={"100%"}
+        margin={0}
+        inset="0"
+        border="2px solid red"
+        pos={{ base: "fixed", xmd: "static" }}>
         <Home session={session} />
         {children}
       </Flex>
